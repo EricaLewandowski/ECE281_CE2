3ce768aadf880f00dfd83ef5e306e6d40c7f22a5
EricaLewandowski <C16erica.lewandowski@usafa.edu>
2014-02-06 14:34:23 -0700

    added all file

---
+++ Decoder_behavioral.vhd
@@ -0,0 +1,57 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:22:30 02/06/2014 
+-- Design Name: 
+-- Module Name:    Decoder_behavioral - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Decoder_behavioral is
+    Port ( I0 : in  STD_LOGIC;
+           I1 : in  STD_LOGIC;
+           EN : in  STD_LOGIC;
+           Y0 : out  STD_LOGIC;
+           Y1 : out  STD_LOGIC;
+           Y2 : out  STD_LOGIC;
+           Y3 : out  STD_LOGIC);
+end Decoder_behavioral;
+
+architecture Behavioral of Decoder_behavioral is
+
+signal I0_Not, I1_Not : std_logic;
+
+begin
+
+I0_Not <= not I0;
+I1_Not <= not I1;
+
+Y0 <= I0_Not and I1_Not and EN;
+Y1 <= I0 and I1_Not and EN;
+Y2 <= I0_Not and I1 and EN;
+Y3 <= I0 and I1 and EN;
+
+end Behavioral;
+
